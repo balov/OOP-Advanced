ecb570182ac7376189dfee59935759add764acd0
Aleksandar Balov <alexandre.balov@gmail.com>
2017-10-01 07:14:33 -0700

    Add files via upload

---
+++ 1.InterfaceAndAbstractionLab/Car/Entities/Tesla.cs
@@ -0,0 +1,30 @@
+﻿public class Tesla : ICar, IElectricCar
+{
+    public int Battery { get; }
+
+    public string Color { get; }
+
+    public string Model { get; }
+
+    public Tesla(int battery, string color, string model)
+    {
+        this.Battery = battery;
+        this.Color = color;
+        this.Model = model;
+    }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak";
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
---
+++ 1.InterfaceAndAbstractionLab/Car/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        ICar seat = new Seat("Leon", "Grey");
+        ICar tesla = new Tesla(3, "Red", "Model 3");
+
+        Console.WriteLine(seat.ToString());
+        Console.WriteLine(tesla.ToString());
+    }
+}
