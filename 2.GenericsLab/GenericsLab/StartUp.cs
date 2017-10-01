45d11e9bfcb35238cb0686ec1c73b2805560e1c3
Aleksandar Balov <alexandre.balov@gmail.com>
2017-10-01 07:23:15 -0700

    Add files via upload

---
+++ 2.GenericsLab/GenericsLab/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        Box<int> box = new Box<int>();
+        box.Add(1);
+        box.Add(2);
+        box.Add(3);
+        Console.WriteLine(box.Remove());
+        box.Add(4);
+        box.Add(5);
+        Console.WriteLine(box.Remove());
+
+        string[] strings = ArrayCreator.Create(5, "Pesho");
+        int[] integers = ArrayCreator.Create(10, 33);
+    }
+}
